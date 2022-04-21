bfe31ba2b309fa8d94f987285457f06227b1b560
fcesc-code <bpfran@gmail.com>
2022-04-21 08:10:38 +0200

    config / create config to acces firebase database

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
+import { DB_URI, FB_KEYS } from './KEYS/FIREBASE_KEYS';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService = app.get(ConfigService);
+  const adminConfig: ServiceAccount = {
+    projectId: configService.get<string>(FB_KEYS.project_id),
+    privateKey: configService
+      .get<string>(FB_KEYS.private_key)
+      .replace(/\\n/g, '\n'),
+    clientEmail: configService.get<string>(FB_KEYS.client_email),
+  };
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+    databaseURL: DB_URI,
+  });
+
+  app.enableCors();
+
+  await app.listen(configService.get<string>('API_SYNC_PORT') || 4000);
 }
 bootstrap();
